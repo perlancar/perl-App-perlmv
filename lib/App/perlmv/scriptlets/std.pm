cecb117b5ec824d6ef30a219f9bcc03428066fb8
Steven Haryanto <steven@masterweb.net>
2010-05-13 10:36:57 +0700

    add a simple microformat to scriptlet; add scriptlet 'unaccent'

--- lib/App/perlmv/scriptlets/std.pm
+++ lib/App/perlmv/scriptlets/std.pm
@@ -3,20 +3,24 @@ package App::perlmv::scriptlets::std;
 our %scriptlets = (
 
 
-    'lc' => "# Convert filenames to lowercase\n" . q[$_ = lc],
+
+    'lc' => "### Summary: Convert filenames to lowercase\n" . q[$_ = lc],
 
 
-    'uc' => "# Convert filenames to uppercase\n" . q[$_ = uc],
+
+    'uc' => "### Summary: Convert filenames to uppercase\n" . q[$_ = uc],
 
 
-    'with-numbers' => "# Rename files into numbers, e.g. (file1, foo, quux, qux) -> (1, 2, 3, 4)\n".
+
+    'with-numbers' => "### Summary: Rename files into numbers, e.g. (file1, foo, quux, qux) -> (1, 2, 3, 4)\n".
 q{$i||=0; $i++ unless $TESTING;
 /.+\.(.+)/; $ext=$1;
 $ndig = @ARGV >= 1000 ? 4 : @ARGV >= 100 ? 3 : @ARGV >= 10 ? 2 : 1;
 $_ = sprintf "%0${ndig}d%s", $i, (defined($ext) ? ".$ext" : "");'},
 
 
-    'remove-common-prefix' => "# Remove prefix that are common to all args, e.g. (file1, file2b) -> (1, 2b)\n".
+
+    'remove-common-prefix' => "### Summary: Remove prefix that are common to all args, e.g. (file1, file2b) -> (1, 2b)\n".
 q{
 if (!defined($COMMON_PREFIX)) {
     for ($i=0; $i<length($ARGV[0]); $i++) {
@@ -27,7 +31,8 @@ if (!defined($COMMON_PREFIX)) {
 s/^\Q$COMMON_PREFIX//;},
 
 
-    'remove-common-suffix' => "# Remove suffix that are common to all args, e.g. (1.txt, a.txt) -> (1, a)\n".
+
+    'remove-common-suffix' => "### Summary: Remove suffix that are common to all args, e.g. (1.txt, a.txt) -> (1, a)\n".
 q{if (!defined($COMMON_SUFFIX)) {
     for (@ARGV) { $_ = reverse };
     for ($i=0; $i<length($ARGV[0]); $i++) {
@@ -41,8 +46,17 @@ q{if (!defined($COMMON_SUFFIX)) {
 s/\Q$COMMON_SUFFIX\E$/$EXT/;},
 
 
-    'pinyin' => "# Rename Chinese characters in filename into their pinyin\n".
+
+    'pinyin' => "### Summary: Rename Chinese characters in filename into their pinyin\n".
+                "### Requires: Lingua::Han::Pinyin\n".
 q{use Lingua::Han::PinYin; $h||=Lingua::Han::PinYin->new; $_=$h->han2pinyin($_)},
+
+
+
+    'unaccent' => "### Summary: Remove accents in filename, e.g. accéder.txt -> acceder.txt\n".
+                "### Requires: Text::Unaccent::PurePerl\n".
+q{use Text::Unaccent::PurePerl; $_ = unac_string("UTF8", $_)},
+
 
 
 );
